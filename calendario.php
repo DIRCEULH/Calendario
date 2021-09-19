2e075c59b150c46f111a638834bf06e0f597b217
Dirceu102650 <dirceulh@hotmail.com>
2021-09-19 10:29:47 -0300

    commit 4

--- calendario.php
+++ calendario.php
@@ -1,43 +1,58 @@
 <?php
 
 // Adicionar zero antes do dia menor que 10
-    function numero($num){
+    function numero($num)
+    {
         return ($num < 10) ? '0'.$num : $num;
     }
     // função para pegar os eventos do banco de dados
-    function montaEventos($info){
+    function montaEventos($info)
+    {
         global $pdo;
         //tabela, data, titulo
         $tabela = $info['tabela'];
+
         $data = $info['data'];
+
         $titulo = $info['titulo'];
+
         $mensagem = $info['mensagem'];
 
         $eventos = $pdo->prepare("SELECT * FROM ".$tabela." ");
+
         $eventos->execute();
 
         $retorno = array();
-        while($row = $eventos->fetchObject()){
+
+        while($row = $eventos->fetchObject())
+        {
             $dataArr = date('Y-m-d', strtotime($row->{$data}));
+
             $retorno[$dataArr] = array(
+
                 'titulo' => $row->{$titulo},
+
                 'mensagem' => $row->{$mensagem}
             );
         }
+
         return $retorno;
     }
     // funcao para Retorna o número de dias em um mês de um calendário e ano requisitado
-    function diasMeses(){
+    function diasMeses()
+    {
         $retorno = array();
 
-        for($i = 1; $i<=12;$i++){
+        for($i = 1; $i<=12;$i++)
+        {
             $retorno[$i] = cal_days_in_month(CAL_GREGORIAN, $i, date('Y'));
         }
 
         return $retorno;
     }
 // funcao para montar calendario com os dias da semana e nome dos meses
-    function montaCalendario($eventos = array()){
+    function montaCalendario($eventos = array())
+    {
         $daysWeek = array(
             'Sun',
             'Mon',
@@ -71,59 +86,88 @@
             12 => 'Dezembro'
         );
         $diasMeses = diasMeses();
+
         $arrayRetorno = array();
 
-        for($i =1; $i <= 12; $i++){
+        for($i =1; $i <= 12; $i++)
+        {
             $arrayRetorno[$i] = array();
-            for($n=1; $n<= $diasMeses[$i]; $n++){
+
+            for($n=1; $n<= $diasMeses[$i]; $n++)
+            {
                 $dayMonth = gregoriantojd($i, $n, date('Y'));
+
                 $weekMonth = substr(jddayofweek($dayMonth, 1),0,3);
+
                 if($weekMonth == 'Mun') $weekMonth = 'Mon';
+
                 $arrayRetorno[$i][$n] = $weekMonth;
             }
         }
         echo '<a href="#" id="volta">&laquo;</a><a href="#" id="vai">&raquo;</a>';
+
         echo '<table border="0" width="100%">';
-        foreach($arrayMes as $num => $mes){
+
+        foreach($arrayMes as $num => $mes)
+        {
             echo '<tbody id="mes_'.$num.'" class="mes">';
             echo '<tr class="mes_title"><td colspan="7">'.$mes.'</td></tr>';
             echo '<tr class="dias_title">';
-            foreach($diasSemana as $i => $day){
+            foreach($diasSemana as $i => $day)
+            {
                 echo '<td>'.$day.'</td>';
             }
+
             echo '</tr><tr>';
+
             $y = 0;
-            foreach($arrayRetorno[$num] as $numero => $dia){
+
+            foreach($arrayRetorno[$num] as $numero => $dia)
+            {
                 $y++;
-                if($numero == 1){
+
+                if($numero == 1)
+                {
                     $qtd = array_search($dia, $daysWeek);
                     for($i=1; $i<=$qtd; $i++){
                         echo '<td></td>';
                         $y+=1;
                     }
                 }
-                if(count($eventos) > 0){
+                if(count($eventos) > 0)
+                {
                     $month = numero($num);
                     $dayNow = numero($numero);
                     $date = date('Y').'-'.$month.'-'.$dayNow;
-                    if(in_array($date, array_keys($eventos))){
+                    if(in_array($date, array_keys($eventos)))
+                    {
                         $evento = $eventos[$date];
+
                         echo '<td class="evento"><a href="eventos.php?data='.date('d/m/Y', strtotime($date )).'&&titulo='.$evento['titulo'].'&&mensagem='.$evento['mensagem'].' " title="'.$evento['titulo'].'-'.$evento['mensagem'].'">'.$numero.'</a><a style="margin-top:-40px; margin-left:px;" href="listaeventos.php?data='.date('d/m/Y', strtotime($date )).'" title=""><i class="fa fa-calendar-check-o" aria-hidden="true"></i></a></td>';
+                   
                     }else{
-             echo '<td class="dia_'.$numero.'"><a href="eventos.php?data='.date('d/m/Y ', strtotime($date )).'" title="">'.$numero.'</a></td>';
+
+                       echo '<td class="dia_'.$numero.'"><a href="eventos.php?data='.date('d/m/Y ', strtotime($date )).'" title="">'.$numero.'</a></td>';
                        
                      
                     }
-                }else{
+                }
+                else
+                {
+
                     echo '<td class="dia_'.$numero.'"><a href="eventos.php?data=2021-09-30" title="">'.$numero.'</a></td>';
                 }
-                if($y == 7){
+                if($y == 7)
+                {
                     $y=0;
+
                     echo '</tr><tr>';
                 }
             }
+
             echo '</tr></tbody>';
         }
+        
         echo '</table>';
     }
 ?>
